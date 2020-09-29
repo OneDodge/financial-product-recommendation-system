552131c57532816188ed4056622f5411bf559955
OneDodge <71073232+OneDodge@users.noreply.github.com>
2020-09-29 11:33:08 +0800

    update

--- src/main/python/rs_ds.py
+++ src/main/python/rs_ds.py
@@ -6,58 +6,49 @@ import numpy as np
 # custom class
 from config import Config
 
-# original file columns
-# customer
-USER_COLUMN = 'user'
+# Pre-Processing File Columns
+USER_COLUMN = 'customer'
 AGE_COLUMN = 'age'
 GENDER_COLUMN = 'gender'
-MARITAL_STATUS_COLUMN = 'marital_status'
-HAVE_CHILD_COLUMN = 'have_child'
-EDUCATION_COLUMN = 'education'
+MARITAL_STATUS_COLUMN = 'marital'
+EDUCATION_LEVEL_COLUMN = 'edu_level'
+NUMBER_OF_CHILD_COLUMN = 'num_of_child'
+RISK_LEVEL_COLUMN = 'risk_level'
+TOTAL_TCR_COLUMN = 'total_tcr'
+SALARY_COLUMN = 'salary'
+SECURITY_CODE_COLUMN = 'security_code'
+SYMBOL_COLUMN = 'symbol'
+NAME_COLUMN = 'name'
+PRICE_COLUMN = 'price'
+CHANGE_COLUMN = 'change'
+CHANGE_PERCENTAGE_COLUMN = 'change_percentage'
+MARKET_CAPTIAL_COLUMN = 'market_captial'
+TRAILING_P_E_COLUMN = 'trailing_p_e'
+REVENUE_COLUMN = 'revenue'
+VOLUME_COLUMN = 'volume'
+TOTAL_CASH_COLUMN = 'total_cash'
+TOTAL_DEBT_COLUMN = 'total_debt'
+FIVE_YEAR_AVERAGE_DIVIDEND_YIELD = '5_year_average_dividend_yield'
+SECTOR_COLUMN = 'sector'
+INDUSTRY_COLUMN = 'industry'
 
-# product
-PRODUCT_COLUMN = 'product_name'
-PRODUCT_3_YR_RETURN_COLUMN = '3year_return'
-PRODUCT_STD_DEV_COLUMN = 'standard_deviation'
-PRODUCT_DEVIDEND_COLUMN = 'dividend'
-PRODUCT_ASSET_CLASS_COLUMN = 'asset_class'
-
-# pre-process column
-# customer
-AGE_CATEGORY_COLUMN = 'age_category'
-
-# index column
-# customer
-USER_INDEX_COLUMN = 'user_index'
-AGE_INDEX_COLUMN = 'age_index'
-GENDER_INDEX_COLUMN = 'gender_index'
-MARITAL_STATUS_INDEX_COLUMN = 'marital_status_index'
-HAVE_CHILD_INDEX_COLUMN = 'have_child_index'
-EDUCATION_INDEX_COLUMN = 'education_index'
-
-# product
+USER_INDEX_COLUMN = 'customer_index'
 PRODUCT_INDEX_COLUMN = 'product_index'
-PRODUCT_ASSET_CLASS_INDEX_COLUMN = 'asset_class_index'
 
-IN_HEADERS = [USER_COLUMN, AGE_COLUMN, GENDER_COLUMN, MARITAL_STATUS_COLUMN, HAVE_CHILD_COLUMN, EDUCATION_COLUMN,
-              PRODUCT_COLUMN, PRODUCT_3_YR_RETURN_COLUMN, PRODUCT_STD_DEV_COLUMN, PRODUCT_DEVIDEND_COLUMN, PRODUCT_ASSET_CLASS_COLUMN]
-
-OUT_HEADERS = [USER_COLUMN, AGE_COLUMN, GENDER_COLUMN, MARITAL_STATUS_COLUMN, HAVE_CHILD_COLUMN, EDUCATION_COLUMN,
-               PRODUCT_COLUMN, PRODUCT_3_YR_RETURN_COLUMN, PRODUCT_STD_DEV_COLUMN, PRODUCT_DEVIDEND_COLUMN, PRODUCT_ASSET_CLASS_COLUMN,
-               AGE_CATEGORY_COLUMN,
-               USER_INDEX_COLUMN,
-               AGE_INDEX_COLUMN,
-               GENDER_INDEX_COLUMN,
-               EDUCATION_INDEX_COLUMN,
-               HAVE_CHILD_INDEX_COLUMN,
-               MARITAL_STATUS_INDEX_COLUMN,
-               PRODUCT_INDEX_COLUMN,
-               PRODUCT_ASSET_CLASS_INDEX_COLUMN]
+IN_HEADERS = [USER_COLUMN, AGE_COLUMN, GENDER_COLUMN, MARITAL_STATUS_COLUMN, EDUCATION_LEVEL_COLUMN, NUMBER_OF_CHILD_COLUMN,
+              RISK_LEVEL_COLUMN, TOTAL_TCR_COLUMN, SALARY_COLUMN, SECURITY_CODE_COLUMN, SYMBOL_COLUMN, NAME_COLUMN, PRICE_COLUMN, CHANGE_COLUMN,
+              CHANGE_PERCENTAGE_COLUMN, MARKET_CAPTIAL_COLUMN,
+              #   TRAILING_P_E_COLUMN,
+              #   REVENUE_COLUMN,
+              VOLUME_COLUMN,
+              #   TOTAL_CASH_COLUMN,
+              #   TOTAL_DEBT_COLUMN,
+              FIVE_YEAR_AVERAGE_DIVIDEND_YIELD, SECTOR_COLUMN, INDUSTRY_COLUMN]
 
 PROBABILITY_COLUMN = 'probability (%)'
 
 # read file
-in_df = pd.read_csv(Config.getNNFileInput(),
+in_df = pd.read_csv(Config.getNNProcessingFileInput(),
                     sep=",",
                     names=IN_HEADERS,
                     header=0,
@@ -66,42 +57,27 @@ in_df = pd.read_csv(Config.getNNFileInput(),
     AGE_COLUMN: np.int32,
     GENDER_COLUMN: 'str',
     MARITAL_STATUS_COLUMN: 'str',
-    HAVE_CHILD_COLUMN: 'str',
-    EDUCATION_COLUMN: 'str',
-    PRODUCT_COLUMN: 'str',
-    PRODUCT_3_YR_RETURN_COLUMN: np.float64,
-    PRODUCT_STD_DEV_COLUMN: np.float64,
-    PRODUCT_DEVIDEND_COLUMN: np.float64,
-    PRODUCT_ASSET_CLASS_COLUMN: 'str'
+    EDUCATION_LEVEL_COLUMN: 'str',
+    NUMBER_OF_CHILD_COLUMN: np.int32,
+    RISK_LEVEL_COLUMN: np.int32,
+    TOTAL_TCR_COLUMN: np.float64,
+    SALARY_COLUMN: np.float64,
+    SECURITY_CODE_COLUMN: 'str',
+    SYMBOL_COLUMN: 'str',
+    NAME_COLUMN: 'str',
+    PRICE_COLUMN: np.float64,
+    CHANGE_COLUMN: np.float64,
+    CHANGE_PERCENTAGE_COLUMN: np.float64,
+    MARKET_CAPTIAL_COLUMN: np.float64,
+    # TRAILING_P_E_COLUMN: np.float64,
+    # REVENUE_COLUMN: np.float64,
+    VOLUME_COLUMN: np.float64,
+    # TOTAL_CASH_COLUMN: np.float64,
+    # TOTAL_DEBT_COLUMN: np.float64,
+    FIVE_YEAR_AVERAGE_DIVIDEND_YIELD: np.float64,
+    SECTOR_COLUMN: 'str',
+    INDUSTRY_COLUMN: 'str'
 })
-
-# read file
-# out_df = pd.read_csv(Config.getNNFileOutput(),
-#                      sep=",",
-#                      names=OUT_HEADERS,
-#                      header=0,
-#                      dtype={
-#                      USER_COLUMN: 'str',
-#                      AGE_COLUMN: np.int32,
-#                      GENDER_COLUMN: 'str',
-#                      MARITAL_STATUS_COLUMN: 'str',
-#                      HAVE_CHILD_COLUMN: 'str',
-#                      EDUCATION_COLUMN: 'str',
-#                      PRODUCT_COLUMN: 'str',
-#                      PRODUCT_3_YR_RETURN_COLUMN: np.float64,
-#                      PRODUCT_STD_DEV_COLUMN: np.float64,
-#                      PRODUCT_DEVIDEND_COLUMN: np.float64,
-#                      PRODUCT_ASSET_CLASS_COLUMN: 'str',
-#                      AGE_CATEGORY_COLUMN: 'str',
-#                      USER_INDEX_COLUMN: np.int32,
-#                      AGE_INDEX_COLUMN: np.int32,
-#                      GENDER_INDEX_COLUMN: np.int32,
-#                      EDUCATION_INDEX_COLUMN: np.int32,
-#                      HAVE_CHILD_INDEX_COLUMN: np.int32,
-#                      MARITAL_STATUS_INDEX_COLUMN: np.int32,
-#                      PRODUCT_INDEX_COLUMN: np.int32,
-#                      PRODUCT_ASSET_CLASS_INDEX_COLUMN: np.int32
-#                      })
 
 
 class DataStore:
--- src/main/python/rs_nn.py
+++ src/main/python/rs_nn.py
@@ -4,15 +4,12 @@ import pandas as pd
 import numpy as np
 from pandas import HDFStore
 
-from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, Input, Embedding, Concatenate, Multiply, Lambda
 from tensorflow.keras.losses import BinaryCrossentropy, SparseCategoricalCrossentropy, CategoricalCrossentropy
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adagrad, Adam, SGD, RMSprop
 from tensorflow.keras.models import load_model
-from tensorflow.keras import Model, layers
-from tensorflow import feature_column
-
+from tensorflow.keras import Model
 import scipy.sparse as sp
 import argparse
 from absl import flags
@@ -30,18 +27,17 @@ FLAGS = flags.FLAGS
 
 tf.config.threading.set_inter_op_parallelism_threads(12)
 tf.config.threading.set_intra_op_parallelism_threads(12)
-tf.keras.backend.set_floatx('float64')
 
 strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
 
-def get_model(all_user_features, all_product_features, num_users, num_items, latent_dim=8, dense_layers=[64, 32, 16, 8],
+def get_model(num_users, num_items, latent_dim=8, dense_layers=[64, 32, 16, 8],
               reg_layers=[0, 0, 0, 0], reg_mf=0):
 
     # input layer
-    # input_user = Input(shape=(5,), dtype='float64', name='user_input')
-    # input_item = Input(shape=(5,), dtype='float64', name='item_input')
+    input_user = Input(shape=(9,), dtype='float64', name='user_input')
+    input_item = Input(shape=(9,), dtype='float64', name='item_input')
 
     # embedding layer
     mf_user_embedding = Embedding(input_dim=num_users, output_dim=latent_dim,
@@ -66,13 +62,13 @@ def get_model(all_user_features, all_product_features, num_users, num_items, lat
                                    input_length=1)
 
     # MF latent vector
-    # mf_user_latent = Flatten()(mf_user_embedding(user_feature_layer_outputs))
-    # mf_item_latent = Flatten()(mf_item_embedding(product_feature_layer_outputs))
-    # mf_cat_latent = Multiply()([mf_user_latent, mf_item_latent])
+    mf_user_latent = Flatten()(mf_user_embedding(input_user))
+    mf_item_latent = Flatten()(mf_item_embedding(input_item))
+    mf_cat_latent = Multiply()([mf_user_latent, mf_item_latent])
 
     # MLP latent vector
-    mlp_user_latent = Flatten()(mlp_user_embedding(all_user_features))
-    mlp_item_latent = Flatten()(mlp_item_embedding(all_product_features))
+    mlp_user_latent = Flatten()(mlp_user_embedding(input_user))
+    mlp_item_latent = Flatten()(mlp_item_embedding(input_item))
     mlp_cat_latent = Concatenate()([mlp_user_latent, mlp_item_latent])
 
     mlp_vector = mlp_cat_latent
@@ -85,57 +81,17 @@ def get_model(all_user_features, all_product_features, num_users, num_items, lat
                       name='layer%d' % i)
         mlp_vector = layer(mlp_vector)
 
-    # predict_layer = Concatenate()([mf_cat_latent, mlp_vector])
+    predict_layer = Concatenate()([mf_cat_latent, mlp_vector])
     # result = Dense(1, activation='softmax',
     #                kernel_initializer='lecun_uniform', name='result')
     result = Dense(1, activation='sigmoid',
                    kernel_initializer='lecun_uniform', name='result')
 
-    model = Model(inputs=[all_user_features, all_product_features],
-                  outputs=result(mlp_vector))
+    model = Model(inputs=[input_user, input_item],
+                  outputs=result(predict_layer))
     return model
 
 # get the training samples
-
-
-def get_category_encoding_layer(name, dataset, dtype, max_tokens=None):
-    # Create a StringLookup layer which will turn strings into integer indices
-    if dtype == 'string':
-        index = preprocessing.StringLookup(max_tokens=max_tokens)
-    else:
-        index = preprocessing.IntegerLookup(max_values=max_tokens)
-
-    # Prepare a Dataset that only yields our feature
-    feature_ds = dataset.map(lambda x, y: x[name])
-
-    # Learn the set of possible values and assign them a fixed integer index.
-    index.adapt(feature_ds)
-
-    # Create a Discretization for our integer indices.
-    encoder = preprocessing.CategoryEncoding(max_tokens=index.vocab_size())
-
-    # Prepare a Dataset that only yields our feature.
-    feature_ds = feature_ds.map(index)
-
-    # Learn the space of possible indices.
-    encoder.adapt(feature_ds)
-
-    # Apply one-hot encoding to our indices. The lambda function captures the
-    # layer so we can use them, or include them in the functional model later.
-    return lambda feature: encoder(index(feature))
-
-
-def get_normalization_layer(name, dataset):
-    # Create a Normalization layer for our feature.
-    normalizer = preprocessing.Normalization()
-
-    # Prepare a Dataset that only yields our feature.
-    feature_ds = dataset.map(lambda x, y: x[name])
-
-    # Learn the statistics of the data.
-    normalizer.adapt(feature_ds)
-
-    return normalizer
 
 
 def get_train_samples(train_mat, num_negatives):
@@ -158,196 +114,150 @@ def get_train_samples(train_mat, num_negatives):
 # An example of the recommendations for financial products
 
 
-TARGET = 'target'
-
-
-def df_to_dataset(dataframe, shuffle=True, batch_size=32):
-    dataframe = dataframe.copy()
-    labels = dataframe.pop(TARGET)
-    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-    if shuffle:
-        ds = ds.shuffle(buffer_size=len(dataframe))
-    ds = ds.batch(batch_size)
-    ds = ds.prefetch(batch_size)
-    return ds
-
-
 def main():
-
-    # hyperparameters
-    verbose = 1
-    epochs = 40
-    batch_size = 256
-    latent_dim = 64
-    dense_layers = [512, 256, 128, 64, 32, 16, 8]
-    reg_layers = [0.000001, 0.000001, 0.000001,
-                  0.000001, 0.000001, 0.000001, 0.000001, 0.000001]
-    reg_mf = 0.000001
-    num_negatives = 2
-    learning_rate = 0.001
-
     # read file
     df = DataStore.getNNFileInput()
 
-    df[ds.USER_INDEX_COLUMN] = df[ds.USER_COLUMN].astype('category').cat.codes
-    df[ds.PRODUCT_INDEX_COLUMN] = df[ds.PRODUCT_COLUMN].astype(
+    # age_bins = [0, 18, 38, 58, 78, 98, np.inf]
+
+    # age_names = ['<18', '18-38', '38-58', '58-78', '78-98', '98+']
+
+    age_bins = [0, 18, 30, 44, 54, 90, np.inf]
+
+    age_names = ['<18', '18-30', '30-44', '44-54', '54-90', '90+']
+
+    df['age_category'] = pd.cut(
+        df[ds.AGE_COLUMN], age_bins, labels=age_names)
+
+    # turn users and products into their unique cat codes so have a 0-N index for users and products
+    df['user_index'] = df[ds.USER_COLUMN].astype('category').cat.codes
+    df['age_index'] = df['age_category'].astype(
+        'category').cat.codes
+    df['gender_index'] = df[ds.GENDER_COLUMN].astype(
+        'category').cat.codes
+    df['edu_level_index'] = df[ds.EDUCATION_LEVEL_COLUMN].astype(
+        'category').cat.codes
+    df['marital_status_index'] = df[ds.MARITAL_STATUS_COLUMN].astype(
         'category').cat.codes
 
-    df[TARGET] = 1
+    df['symbol_index'] = df[ds.SYMBOL_COLUMN].astype(
+        'category').cat.codes
+    df['sector_index'] = df[ds.SECTOR_COLUMN].astype(
+        'category').cat.codes
+    df['industry_index'] = df[ds.INDUSTRY_COLUMN].astype(
+        'category').cat.codes
 
-    x_train = df[ds.USER_INDEX_COLUMN]
-    y_train = df[ds.PRODUCT_INDEX_COLUMN]
+    x_train = df['user_index']
+    y_train = df['symbol_index']
 
+    # save data in dok matrix(optimized sparse matrix object)
+    # create a sparse portfolio x products matrix
+    # if a user i has product j, mat[i, j] = 1
     mat_train = sp.dok_matrix((x_train.shape[0], len(
         y_train.unique())), dtype=np.float32)
     for userIndex, productIndex in zip(x_train, y_train):
         mat_train[userIndex, productIndex] = 1
+
+    # hyperparameters
+    # loaded = True
+    verbose = 1
+    epochs = 40
+    batch_size = 256
+    latent_dim = 64
+    # dense_layers = [64, 32, 16, 8]
+    # reg_layers = [0, 0, 0, 0]
+    # dense_layers = [64, 32, 16, 8]
+    # reg_layers = [0.000001, 0.000001, 0.000001, 0.000001]
+    dense_layers = [512, 256, 128, 64, 32, 16, 8]
+    reg_layers = [0.000001, 0.000001, 0.000001,
+                  0.000001, 0.000001, 0.000001, 0.000001, 0.000001]
+    reg_mf = 0.000001
+    # reg_mf = 0
+    num_negatives = 2
+    learning_rate = 0.001
+    # learner = 'adam'
+    # dataset = 'portfolio'
+
+    num_users, num_items = mat_train.shape
+    # # # print('Done loading data!')
+
+    # # contstruct the NeuMF Neural Network
+    with strategy.scope():
+        model = get_model((num_users+1)*5, (num_items*3)*5, latent_dim,
+                          dense_layers, reg_layers, reg_mf)
+
+        model.compile(optimizer=Adam(lr=learning_rate),
+                      loss=BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+    print(model.summary())
+
     user_input_train, item_input_train, labels_train = get_train_samples(
         mat_train, num_negatives)
 
-    new_table = []
-    for i in range(len(user_input_train)):
-        user_df = df[df[ds.USER_INDEX_COLUMN] == user_input_train[i]]
-        product_df = df[df[ds.PRODUCT_INDEX_COLUMN]
-                        == item_input_train[i]].iloc[[0]]
-        label = labels_train[i]
+    # organise training input<User,Product><Label>
+    modified_x_train = []
+    for ui in user_input_train:
+        user_df = df[df['user_index'] == ui]
+        modified_x_train_element = []
+        modified_x_train_element.append(ui)
+        modified_x_train_element.append(
+            user_df['age_index'].to_numpy()[0])
+        modified_x_train_element.append(
+            user_df['gender_index'].to_numpy()[0])
+        modified_x_train_element.append(
+            user_df['marital_status_index'].to_numpy()[0])
+        modified_x_train_element.append(
+            user_df['edu_level_index'].to_numpy()[0])
+        modified_x_train_element.append(
+            user_df[ds.NUMBER_OF_CHILD_COLUMN].to_numpy()[0])
+        modified_x_train_element.append(
+            user_df[ds.RISK_LEVEL_COLUMN].to_numpy()[0])
+        # modified_x_train_element.append(
+        #     user_df[ds.TOTAL_TCR_COLUMN].to_numpy()[0])
+        # modified_x_train_element.append(
+        #     user_df[ds.SALARY_COLUMN].to_numpy()[0])
 
-        new_row = []
-        new_row.append(user_df[ds.AGE_COLUMN].to_numpy()[0])
-        new_row.append(user_df[ds.GENDER_COLUMN].to_numpy()[0])
-        new_row.append(user_df[ds.MARITAL_STATUS_COLUMN].to_numpy()[0])
-        new_row.append(user_df[ds.HAVE_CHILD_COLUMN].to_numpy()[0])
-        new_row.append(user_df[ds.EDUCATION_COLUMN].to_numpy()[0])
-        new_row.append(
-            product_df[ds.PRODUCT_3_YR_RETURN_COLUMN].to_numpy()[0])
-        new_row.append(
-            product_df[ds.PRODUCT_STD_DEV_COLUMN].to_numpy()[0])
-        new_row.append(
-            product_df[ds.PRODUCT_DEVIDEND_COLUMN].to_numpy()[0])
-        new_row.append(product_df[ds.PRODUCT_ASSET_CLASS_COLUMN].to_numpy()[0])
-        new_row.append(label)
+        modified_x_train_element.append(1)
+        modified_x_train_element.append(1)
+        modified_x_train.append(modified_x_train_element)
 
-        new_table.append(new_row)
+    modified_y_train = []
+    for ii in item_input_train:
+        product_df = df[df['symbol_index'] == ii]
+        modified_y_train_element = []
+        modified_y_train_element.append(ii)
+        modified_y_train_element.append(
+            product_df[ds.PRICE_COLUMN].to_numpy()[0])
+        # modified_y_train_element.append(
+        #     product_df[ds.CHANGE_COLUMN].to_numpy()[0])
+        # modified_y_train_element.append(
+        #     product_df[ds.CHANGE_PERCENTAGE_COLUMN].to_numpy()[0])
+        # modified_y_train_element.append(
+        #     product_df[ds.MARKET_CAPTIAL_COLUMN].to_numpy()[0])
+        # modified_y_train_element.append(
+        #     product_df[ds.VOLUME_COLUMN].to_numpy()[0])
+        modified_y_train_element.append(
+            product_df[ds.FIVE_YEAR_AVERAGE_DIVIDEND_YIELD].to_numpy()[0])
+        modified_y_train_element.append(
+            product_df['sector_index'].to_numpy()[0])
+        modified_y_train_element.append(
+            product_df['industry_index'].to_numpy()[0])
 
-    new_table = np.array(new_table)
-    df = pd.DataFrame(data=new_table)
-    df.columns = [ds.AGE_COLUMN, ds.GENDER_COLUMN, ds.MARITAL_STATUS_COLUMN, ds.HAVE_CHILD_COLUMN, ds.EDUCATION_COLUMN,
-                  ds.PRODUCT_3_YR_RETURN_COLUMN, ds.PRODUCT_STD_DEV_COLUMN, ds.PRODUCT_DEVIDEND_COLUMN, ds.PRODUCT_ASSET_CLASS_COLUMN, TARGET]
-
-    df[ds.AGE_COLUMN] = df[ds.AGE_COLUMN].astype(str).astype(int)
-    df[ds.PRODUCT_3_YR_RETURN_COLUMN] = df[ds.PRODUCT_3_YR_RETURN_COLUMN].astype(
-        str).astype(float)
-    df[ds.PRODUCT_STD_DEV_COLUMN] = df[ds.PRODUCT_STD_DEV_COLUMN].astype(
-        str).astype(float)
-    df[ds.PRODUCT_DEVIDEND_COLUMN] = df[ds.PRODUCT_DEVIDEND_COLUMN].astype(
-        str).astype(float)
-    df[TARGET] = df[TARGET].astype(str).astype(int)
-
-    train, test = train_test_split(df, test_size=0.2)
-    train, val = train_test_split(train, test_size=0.2)
-    print(len(train), 'train examples')
-    print(len(val), 'validation examples')
-    print(len(test), 'test examples')
-
-    all_user_features_input = []
-    user_encoded_features = []
-
-    all_product_features_input = []
-    product_encoded_features = []
-
-    all_inputs = []
-    encoded_features = []
-
-    batch_size = 256
-    train_ds = df_to_dataset(train, batch_size=batch_size)
-    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-    # Users
-    for header in [ds.AGE_COLUMN]:
-        numeric_col = tf.keras.Input(shape=(1,), name=header)
-        normalization_layer = get_normalization_layer(header, train_ds)
-        encoded_numeric_col = normalization_layer(numeric_col)
-        all_user_features_input.append(numeric_col)
-        user_encoded_features.append(encoded_numeric_col)
-
-        all_inputs.append(numeric_col)
-        encoded_features.append(encoded_numeric_col)
-
-    for header in [ds.GENDER_COLUMN, ds.MARITAL_STATUS_COLUMN, ds.HAVE_CHILD_COLUMN, ds.EDUCATION_COLUMN]:
-        categorical_col = tf.keras.Input(
-            shape=(1,), name=header, dtype='string')
-        encoding_layer = get_category_encoding_layer(header, train_ds, dtype='string',
-                                                     max_tokens=None)
-        encoded_categorical_col = encoding_layer(categorical_col)
-        all_user_features_input.append(categorical_col)
-        user_encoded_features.append(encoded_categorical_col)
-
-        all_inputs.append(categorical_col)
-        encoded_features.append(encoded_categorical_col)
-
-    # Products
-    for header in [ds.PRODUCT_3_YR_RETURN_COLUMN, ds.PRODUCT_STD_DEV_COLUMN, ds.PRODUCT_DEVIDEND_COLUMN]:
-        numeric_col = tf.keras.Input(shape=(1,), name=header)
-        normalization_layer = get_normalization_layer(header, train_ds)
-        encoded_numeric_col = normalization_layer(numeric_col)
-        all_product_features_input.append(numeric_col)
-        product_encoded_features.append(encoded_numeric_col)
-
-        all_inputs.append(numeric_col)
-        encoded_features.append(encoded_numeric_col)
-
-    for header in [ds.PRODUCT_ASSET_CLASS_COLUMN]:
-        categorical_col = tf.keras.Input(
-            shape=(1,), name=header, dtype='string')
-        encoding_layer = get_category_encoding_layer(header, train_ds, dtype='string',
-                                                     max_tokens=None)
-        encoded_categorical_col = encoding_layer(categorical_col)
-        all_product_features_input.append(categorical_col)
-        product_encoded_features.append(encoded_categorical_col)
-
-        all_inputs.append(categorical_col)
-        encoded_features.append(encoded_categorical_col)
-
-    all_user_features = tf.keras.layers.concatenate(user_encoded_features)
-    all_product_features = tf.keras.layers.concatenate(
-        product_encoded_features)
-
-    mlp_user_latent = Flatten()(all_user_features)
-    mlp_item_latent = Flatten()(all_product_features)
-    mlp_cat_latent = Concatenate()([mlp_user_latent, mlp_item_latent])
-
-    mlp_vector = mlp_cat_latent
-
-    # build dense layer for model
-    for i in range(0, len(dense_layers)):
-        layer = Dense(dense_layers[i],
-                      activity_regularizer=l2(reg_layers[i]),
-                      activation='relu',
-                      name='layer%d' % i)
-        mlp_vector = layer(mlp_vector)
-
-    output = Dense(1, activation='sigmoid',
-                   kernel_initializer='lecun_uniform', name='result')(mlp_vector)
-
-    model = Model(inputs=all_inputs,
-                  outputs=output)
-
-    model.compile(optimizer=Adam(lr=learning_rate),
-                  loss=BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
-    print(model.summary())
+        modified_y_train_element.append(1)
+        modified_y_train_element.append(1)
+        modified_y_train_element.append(1)
+        modified_y_train_element.append(1)
+        modified_y_train.append(modified_y_train_element)
 
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=Config.getNNCheckpoint(),
                                                      save_weights_only=True,
                                                      verbose=0)
 
-    model.fit(train_ds, validation_data=val_ds,
-              batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=True, callbacks=[cp_callback])
+    hist = model.fit([np.array(modified_x_train), np.array(modified_y_train)], np.array(labels_train),
+                     batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=True, callbacks=[cp_callback])
 
-    loss, accuracy = model.evaluate(test_ds)
-
-    print("Accuracy", accuracy)
     model.save(Config.getNNModel())
+
+    # df.to_csv(Config.getNNFileOutput(), index=False)
 
 
 if __name__ == '__main__':
